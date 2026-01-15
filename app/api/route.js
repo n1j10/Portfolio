67804ffd77532077a5f75f9242f3a43b4f717159
n1j <n1j@local>
2026-01-15 08:50:30 +0300

    Fix Resend init logic

--- app/api/route.js
+++ app/api/route.js
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-
   const { email, subject, message } = await req.json();
-  
   console.log(email, subject, message);
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  const fromEmail = process.env.FROM_EMAIL;
+
   try {
     const data = await resend.emails.send({
       from: fromEmail,
